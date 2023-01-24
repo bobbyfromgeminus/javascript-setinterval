0923a4e7585223b716dcefe770d6ff7889f67052
bobbyfromgeminus <info.geminus@gmail.com>
2023-01-24 22:39:13 +0100

    2023-01-24 #01

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,17 +2,75 @@
 
 // DEKLARÁCIÓS RÉSZ
 
-// konténer div
-const timerBox = document.querySelector('main div.timer');
+    // számláló dobozok
+    const counterBoxes = document.querySelectorAll('section article div');
+    const firstBox = counterBoxes[0];
+    const secondBox = counterBoxes[1];
 
-// megjelenő idő induló értéke
-let time = 0;
+    // vezérlő gombok
+    const controllerButtons = document.querySelectorAll('section article button');
+    const startButton = controllerButtons[0];
+    const pauseButton = controllerButtons[1];
 
-// Léptető és megjelenítő függvény
-const timerFunction = () => {
-    timerBox.textContent = time;
-    time++;
-}
+    // számláló változók
+    let i = 1;
+    let j = 1;
 
-// Intervallum beállítása - függvény hívás 1000 ms-onként
-setInterval(timerFunction, 1000);
+    // futás/megállítás állapota
+    let isPaused = false;
+
+
+// INTERVALLUM VEZÉRLÉS
+
+    // Léptető és megjelenítő függvény
+    const setStep = () => {
+        firstBox.textContent = i;
+        i++;
+        if (isPaused == false) {
+            secondBox.textContent = j;
+            j++;
+        }
+    }
+
+    // "Indít" gomb elrejtése
+    const hideStartButton = () => {
+        startButton.classList.toggle('hide');
+        pauseButton.classList.toggle('hide');
+    }
+
+    // függvény hívás 1000 ms-onként
+    const intervalStep = () => {
+        setInterval(setStep, 1000)
+    }
+
+    // "Indít" elrejtése és intervallum indítása
+    const startCount = () => {
+        hideStartButton();
+        intervalStep();
+    }
+
+    // "Megállít/Folytat" gomb szín és tartalom módosítása
+    const changeButtonFace = () => {
+        if (isPaused == false) {
+            pauseButton.classList.remove('green');
+            pauseButton.classList.add('orange');
+            pauseButton.textContent = 'Megállít';
+        } else {
+            pauseButton.classList.remove('orange');
+            pauseButton.classList.add('green');
+            pauseButton.textContent = 'Folytat';
+        }
+    }
+
+    // futás/megállítás állapotváltó
+    const changeState = () => {
+        if (isPaused == false) isPaused = true;
+        else isPaused = false;
+        changeButtonFace();
+    }
+
+    // Eseménykezelő az "Indít" gombon
+    startButton.addEventListener('click', startCount);
+
+    // Eseménykezelő a "Megállít" gombon
+    pauseButton.addEventListener('click', changeState);
